1e4336af490449ee95c6beb7d2fc787b6abc4821
assiajsaad <assia.j.saad@gmail.com>
2024-02-21 15:30:52 +0200

    Finished UI and features need touch up

--- script.js
+++ script.js
@@ -1,109 +1,125 @@
+function getComputerChoice() {
+  let computerChoice;
+  let rndInt = Math.floor(Math.random() * 3) + 1;
 
-function getUserChoice() {
-    // let userChoice = prompt('Enter Your Choice to start the game').toLowerCase();
-
-    // return userChoice
+  if (rndInt == 1) {
+    computerChoice = "rock";
+  } else if (rndInt == 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
 }
 
+function playRound(playerSelection, computerSelection) {
+  let result = "";
+  if (playerSelection === computerSelection) {
+    result = "It's a Draw !";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    result = `You win ! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    result = `You win ! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    result = `You win ! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    result = `CPU wins ! ${computerSelection} beats ${playerSelection}`;
+  }
+  return result;
+}
 
-function getComputerChoice (){
-    let computerChoice;
-    let rndInt = Math.floor(Math.random() * 3) + 1;
+function playGame(userInput) {
+  let userChoice;
+  let computerChoice;
+  let result;
 
-    if(rndInt== 1) {
-        computerChoice = 'rock';
-    }else if(rndInt== 2){
-        computerChoice= 'paper';
-    }else{
-        computerChoice='scissors'
+  if (userInput <= 5 || computerPoints <= 5) {
+    userChoice = userInput;
+    computerChoice = getComputerChoice();
+    result = getSecondWord(playRound(userChoice, computerChoice));
+    if (result === "You") {
+      userPoints++;
+    } else if (result === "CPU") {
+      computerPoints++;
     }
-    return computerChoice
+  }
+
+  displayWinner();
+
+  return result;
 }
 
-function playRound (playerSelection, computerSelection) {
-    let result ='';
-    if (playerSelection === computerSelection) {
-        result= "It's a Draw !";
-    }else if(playerSelection == "rock" && computerSelection == "scissors"){
-        result = `You win ! ${playerSelection} beats ${computerSelection}`;
-    }else if(playerSelection == "paper" && computerSelection == "rock") {
-        result = `You win ! ${playerSelection} beats ${computerSelection}`;
-    }else if(playerSelection == "scissors" && computerSelection == "paper") {
-        result = `You win ! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        result = `You Lose ! ${computerSelection} beats ${playerSelection}`;
+function curentScore() {
+    let score = document.querySelector('.score');
+    if(score.lastChild != null){
+        score.removeChild(score.lastChild);
     }
-    return result;
+
+    let currScore = document.createElement("h3");
+    currScore.textContent = `User: ${userPoints} CPU: ${computerPoints}`;
+
+  
+    score.appendChild(currScore);
 }
 
-function playGame() {
-    let userPoints = 0;
-    let computerPoints = 0;
-    let userChoice;
-    let computerChoice;
-    let result;
-    for (let index = 0; index < 5; index++) {
-         userChoice = getUserChoice();
-         computerChoice = getComputerChoice();
-         result = getSecondWord(playRound(userChoice,computerChoice));
-        if (result == "win") {
-            userPoints++;
-        }else if(result == "lose") {
-            computerPoints++;
-        }
-    }
-    if (userPoints == computerPoints) {
-        result = `User = ${userPoints} Computer = ${computerPoints} This is Draw !`
-    } else if (userPoints <= computerPoints) {
-        result = `User = ${userPoints} Computer = ${computerPoints} Computer Wins !`
-    }else {
-        result = `User = ${userPoints} Computer = ${computerPoints} You Win !`
-    }
-    return result;
+function displayWinner() {
+  if (userPoints == 5) {
+    showResult("You Win!");
+    userPoints = 0;
+    computerPoints = 0;
+  } else if (computerPoints == 5) {
+    showResult("CPU Wins!");
+    userPoints = 0;
+    computerPoints = 0;
+  }
 }
 
-function getSecondWord (str){
-    let words = str.split(" ");
-    let secondWord = words[1];
-    return secondWord;
+function getSecondWord(str) {
+  let words = str.split(" ");
+  let secondWord = words[0];
+  return secondWord;
 }
 
-function showResult (result) {
-    let divResult = document.createElement('h3')
-    divResult.textContent = result;
-    divResult.style.cssText = "";
-    
-    document.body.removeChild(document.body.lastChild);
-    document.body.appendChild(divResult)
+function showResult(result) {
+  let divResult = document.createElement("h3");
+  divResult.textContent = result;
+  divResult.style.cssText = "";
+
+  document.body.removeChild(document.body.lastChild);
+  document.body.appendChild(divResult);
 }
 
-let divButtons = document.querySelector('.buttons');
+let divButtons = document.querySelector(".buttons");
 
-divButtons.addEventListener('click',(e)=>{
-    let target = e.target
-    let result= ''
+divButtons.addEventListener("click", (event) => {
+  let target = event.target;
+  let result = "";
 
-    switch (target.id) {
-        case 'rock':
-           result = playRound(target.id,getComputerChoice());
-            showResult(result);
-            break;
-        case 'paper':
-           result = playRound(target.id,getComputerChoice());
-            showResult(result);
-            break;
-        case 'scissors':
-           result = playRound(target.id,getComputerChoice());
-            showResult(result);
-            break;
-    
-        default:
-            break;
-    }
-})
+  switch (target.id) {
+    case "rock":
+      result = playGame(target.id);
+      curentScore();
+      break;
+    case "paper":
+      result = playGame(target.id);
+      curentScore();
+      break;
+    case "scissors":
+      result = playGame(target.id);
+      curentScore();
+      break;
+
+    default:
+      break;
+  }
+});
+
+// Variables
+
+let userPoints = 0;
+let computerPoints = 0;
 
 // Implementation
-// console.log("Welcome to Rock Paper Scissors");
 
 // let result = playGame();
 // console.log(result);
